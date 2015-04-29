985f48a6f8301179d4db74a979fc1402092f57c9
Amir Shavit <amir.shavit@autodesk.com>
2015-04-29 10:00:35 +0300

    Extern methods to customize the visual representations's UI

--- Source-ObjectiveC/SmudgyWindow.h
+++ Source-ObjectiveC/SmudgyWindow.h
@@ -14,4 +14,28 @@
  */
 @interface SmudgyWindow : UIWindow
 
+/**
+ *  Customize the way the visual representations are displayed on screen
+ *  @param newRadius      Size of the visual representation
+ *  @param newLineWidth   Width of the border line of the visual representation
+ *  @param newFillColor   Color of the visual representation body
+ *  @param newStrokeColor Color of the border line of the visual representation body
+ */
+- (void)updateLayerRadius:(CGFloat)newRadius
+                lineWidth:(CGFloat)newLineWidth
+                fillColor:(UIColor*)newFillColor
+              strokeColor:(UIColor*)newStrokeColor;
+
+/**
+ *  Customize the shadow of the visual representations
+ *  @param newShadowColor   Color of the visual representation's shadow
+ *  @param newShadowRadius  Spread radius of the visual representations's shadow
+ *  @param newShadowOpacity Opacity of the visual representations's shadow
+ *  @param newShadowOffset  Offset of the visual representations's shadow
+ */
+- (void)updateShadowColor:(UIColor*)newShadowColor
+             shadowRadius:(CGFloat)newShadowRadius
+            shadowOpacity:(CGFloat)newShadowOpacity
+             shadowOffset:(CGSize)newShadowOffset;
+
 @end
